8c8dbd3a38bcc5a5ee4b5174f5f0d3592bc28b87
Marco-Congedo <marco.congedo@gmail.com>
2019-04-22 08:58:01 +0200

    fixed doc make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,21 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, PosDefManifold
 
-makedocs(sitename="PosDefManifold", pages =
-[ "index.md", "introToRiemannianGeometry.md", "MainModule.md", "riemannianGeometry.md",
- "linearAlgebra.md", "signalProcessing.md", "test.md"])
+makedocs (
+   sitename="PosDefManifold",
+   modules=[PosDefManifold],
+   pages =
+   [
+      "index.md",
+      "introToRiemannianGeometry.md",
+      "MainModule.md",
+      "riemannianGeometry.md",
+      "linearAlgebra.md",
+      "signalProcessing.md",
+      "test.md"
+   ]
+)
 
- deploydocs(
-    repo = "github.com/Marco-Congedo/PosDefManifold.jl.git")
+deploydocs(
+    repo = "github.com/Marco-Congedo/PosDefManifold.jl.git"
+)
