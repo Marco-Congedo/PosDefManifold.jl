cdff0fdd1ee7a52abdcb84000ed3767133d465e7
Marco-Congedo <marco.congedo@gmail.com>
2019-11-06 16:50:46 +0100

    Updated function vecP

--- src/signalProcessing.jl
+++ src/signalProcessing.jl
@@ -1,5 +1,5 @@
 #    Unit signalProcessing.jl, part of PosDefManifold Package for julia language
-#    v 0.3.4 - last update 5th of September 2019
+#   v 0.3.5 - last update 6th of November 2019
 #
 #    MIT License
 #    Copyright (c) 2019, Marco Congedo, CNRS, Grenobe, France:
