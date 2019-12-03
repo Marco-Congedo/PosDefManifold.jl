8452d7e0d610c815a833df5bc6ad2b4cd209396a
Marco-Congedo <marco.congedo@gmail.com>
2019-12-03 15:18:34 +0100

    Recreated v0.3.6

--- src/PosDefManifold.jl
+++ src/PosDefManifold.jl
@@ -1,5 +1,5 @@
 #   Main Module of the  PosDefManifold Package for julia language
-#   v 0.3.9 - last update 3rd of December 2019
+#   v 0.3.6 - last update 3rd of December 2019
 #
 #   MIT License
 #   Copyright (c) 2019, Marco Congedo, CNRS, Grenobe, France:
