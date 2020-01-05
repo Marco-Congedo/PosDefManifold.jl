0b399af24aac0e2c9e4b70c1dc48eb4e052738bd
Marco-Congedo <marco.congedo@gmail.com>
2020-01-05 11:10:07 +0100

    fixed function `quadraticForm`

--- src/linearAlgebra.jl
+++ src/linearAlgebra.jl
@@ -852,7 +852,7 @@ tr(H::Union{ℍ{T}, 𝕄{T}}, D::𝔻{T}) where T<:RealOrComplex = tr(D, H)
     (1) quadraticForm(v::Vector{T}, P::ℍ{T}) where T<:Real
     (2) quadraticForm(v::Vector{T}, L::𝕃{T}) where T<:Real
     (3) quadraticForm(v::Vector{T}, X::𝕄{T}, forceLower::Bool=false) where T<:Real
-    (4) quadraticForm(v::Vector{S}, X::Union{𝕄{S}, ℍ{S}}) where S<:Complex
+    (4) quadraticForm(v::Vector{S}, X::Union{𝕄{S}, ℍ{S}, 𝕃{S}}) where S<:Complex
 
  **alias**: `qf`
 
@@ -861,19 +861,20 @@ tr(H::Union{ℍ{T}, 𝕄{T}}, D::𝔻{T}) where T<:RealOrComplex = tr(D, H)
 
  ``v^TPv``,
 
- where the superscript *T* denotes transpose,
- using only the lower triangular part of ``P``.
+ where the superscript *T* denotes transpose.
+ It uses only the lower triangular part of ``P``.
 
  (2) As in (1), given a real vector ``v``
- and the `LowerTriangular` view ``L`` of a real symmetric matrix.
+ and a `LowerTriangular` matrix ``L``.
 
  (3) As in (1), given a real vector ``v``
  and a real generic `Matrix` ``M``, if `forceLower=true`. If `forceLower=false`,
- the product ``v^TMv`` is evaluated using the whole matrix ``M``.
+ the product ``v^TMv`` is evaluated instead using the whole matrix ``M``.
 
  (4) Quadratic form ``v^HPv``, where superscript *H* denotes complex conjugate
- and transpose, for a complex vector `v` and  `Matrix` or `Hermitian` matrix.
- The whole matrix is then used.
+ and transpose, for a complex vector `v` and a complex `Matrix`, `LowerTrianglar`
+ or `Hermitian` matrix.
+ The whole matrix is used.
 
  ## Math
 
@@ -881,32 +882,56 @@ tr(H::Union{ℍ{T}, 𝕄{T}}, D::𝔻{T}) where T<:RealOrComplex = tr(D, H)
 
  ``\\sum_i(v_i^2x_{ii})+\\sum_{i>j}(2v_iv_jx_{ij})``.
 
- This is used in (1), (2) and (3).
+ For ``L`` lower triangular is
+
+ ``\\sum_i(v_i^2x_{ii})+\\sum_{i>j}(v_iv_jx_{ij})``.
+
+ These formula are used in methods (1), (2) and (3).
 
 
  ## Examples
     using PosDefManifold
     P=randP(5) # generate a random real positive definite matrix 5x5
     v=randn(5)
-    q1=quadraticForm(v, P) # or q1=quad(v, P)
-    # obtain a lower Triangular view of P
-    L=LowerTriangular(Matrix(P)) # or L=𝕃(Matrix(P))
-    q2=quadraticForm(v, L)
+    q1=quadraticForm(v, P) # or q1=qf(v, P)
+	q2=v'*P*v
     q1 ≈ q2 ? println(" ⭐ ") : println(" ⛔ ")
 """
-quadraticForm(v::Vector{T}, P::ℍ{T}) where T<:Real = v'*P*v
+function quadraticForm(v::Vector{T}, P::ℍ{T}) where T<:Real
+	r=length(v)
+    s=T(0)
+    for j=1:r-1
+        @inbounds s+=(v[j]^2 * P[j, j])
+        for i=j+1:r @inbounds s+=2*v[i]*v[j]*P[i, j]  end
+    end
+    @inbounds s+=(v[r]^2 * P[r, r])
+    return s
+end
 
-quadraticForm(v::Vector{T}, X::𝕄{T}, forceLower::Bool=false) where T<: Real =
-	forceLower==true ? qf(v, 𝕃(X)) : v'*X*v
+function quadraticForm(v::Vector{T}, X::𝕄{T},
+	                   forceLower::Bool=false) where T<: Real
+	if forceLower
+		r=length(v)
+		s=T(0)
+		for j=1:r-1
+			@inbounds s+=(v[j]^2 * X[j, j])
+			for i=j+1:r @inbounds s+=2*v[i]*v[j]*X[i, j]  end
+		end
+		@inbounds s+=(v[r]^2 * X[r, r])
+		return s
+	else
+		return v'*X*v
+	end
+end
 
-quadraticForm(v::Vector{T}, X::Union{𝕄{T}, ℍ{T}}) where T<:Complex = v'*X*v
+quadraticForm(v::Vector{T}, X::Union{𝕄{T}, ℍ{T}, 𝕃{T}}) where T<:Complex = v'*X*v
 
 function quadraticForm(v::Vector{T}, L::𝕃{T}) where T<:Real
     r=length(v)
     s=T(0)
     for j=1:r-1
         @inbounds s+=(v[j]^2 * L[j, j])
-        for i=j+1:r @inbounds s+=2*v[i]*v[j]*L[i, j]  end
+        for i=j+1:r @inbounds s+=v[i]*v[j]*L[i, j]  end
     end
     @inbounds s+=(v[r]^2 * L[r, r])
     return s
